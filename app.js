72622a754916300fc456b354fe832755d07a55f1
visidevi <32283668+visidevi@users.noreply.github.com>
2017-10-05 18:13:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
-//preguntar el numero de la targeta 
-var targetNum = prompt('ingresa tu numero de Tarjeta');
-var str = targetNum;
-var correctNum=[];//Arreglo vacio, para pushear los datos 
+function isValid(){
+  var u = false;
+   do{
+		var targetNum = prompt('Ingresa el numero de tu Tarjeta');
+		for (i=0;i<targetNum.length;i++){
+ 		if ( targetNum[i]===' '){
+ 			u = true;
+ 			alert('Ingrese un numero sin espacios Vacios');
+		    break;
+		} 
+	}
+	}while( u === true);
+
+  //preguntar el numero de la targeta pasar orden inverso
+    var str = targetNum;
+    var correctNum=[];//Arreglo vacio, para pushear el numero ingresado en orden Inverso. 
 //comprobar que el numero de la tarjeta si es un numero, y no tiene espacios vacios
-//y son solo 9 dijitos.
 
 //retonar el numero del array alrevez
-var res = str.split("");
-for (var i = 0; i < res.length; i++) {
-  var item = res.pop();
-  res.splice(i, 0, item);
-}
-for(i=0;i< res.length;i++){
-  correctNum.push(parseInt(res[i]));
-  }
-//for para volver numeros
-//Preguntar numero de tarjeta y pasar orden inverso ARRIBA BIENNN
-// SACAR NUMERO  DE POSICIONES PARES E IMPARES 
-console.log (correctNum);//numero de  alrevez
-var suma = 0;
-var pairs=0;
-a=[];
-for (var i=0;i<correctNum.length; i++) {
-      if(correctNum[i]%2!== 0){
-       suma += (correctNum[i]);
-       }else if (correctNum[i]%2=== 0){
-         if(correctNum[i]*2>=10){
-          suma+=((correctNum[i]%10)+(parseInt(correctNum[i]/10)));
-           }else{          
-            suma+=(correctNum[i]*2);
+    var res = str.split("");
+    for (var i = 0; i < res.length; i++) {
+          var item = res.pop();
+          res.splice(i, 0, item);
+    }
+//for para volver  el str ingresado a un Array de numeros
+    for(i=0;i< res.length;i++){
+          correctNum.push(parseInt(res[i]));
+    }
+
+//___________________________________________________________-
+// SACAR NUMERO  DE POSICIONES PARES E IMPARES y ejecutar algoritmo de Luhn.
+//corregir!!!
+
+    console.log (correctNum);
+    for (var j=0;j<correctNum.length; j++) {
+        var suma = 0;
+        var pairs=0;
+        if(correctNum[j]%2!== 0){
+        suma += (correctNum[j]);
+        }else if (correctNum[j]%2=== 0){
+            if(correctNum[j]*2>=10){
+             //suma+=((correctNum[j]-10)+1)
+            suma+=((correctNum[j]%10)+(parseInt(correctNum[j]/10)));
+            }else{          
+            suma+=(correctNum[j]*2);
             }
          }
-
+         
+    if ((suma % 10) === 0){
+    alert('VALIDA');
+    }else{
+    alert('inVALIDA');
+    break;
+    }
+  }
 }
-if ((suma % 10) === 0){
-  alert('VALIDA');
-}else{
-alert('inVALIDA');
-}
+isValid()
